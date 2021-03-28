f42a4a40fa9d154acb95dca4437cd96da5ac7521
13ruceYu <bruceyuhongbo@gmail.com>
2021-03-29 07:49:53 +0800

    add: oop practice

--- JavaScript/JS++/业务能力提升-01/02/webpack-basic-config/src/components/InputGroup/index.tpl
+++ JavaScript/JS++/业务能力提升-01/02/webpack-basic-config/src/components/InputGroup/index.tpl
@@ -1,2 +1,2 @@
-<input type="text" class="num-input" placeholder="first number">
-<input type="text" class="num-input" placeholder="second number">
+<input type="text" data-id="fVal" class="num-input" placeholder="first number">
+<input type="text" data-id="sVal" class="num-input" placeholder="second number">
--- JavaScript/JS++/业务能力提升-01/02/webpack-basic-config/src/modules/Calculator.js
+++ JavaScript/JS++/业务能力提升-01/02/webpack-basic-config/src/modules/Calculator.js
@@ -2,34 +2,53 @@ import compute from '../lib/compute';
 import {trimSpace, digitalize} from '../utils/tools';
 
 import ResultComponent from '../components/Result';
+import InputGroupComponent from '../components/InputGroup';
+import BtnGroupComponent from '../components/BtnGroup'
 
 @compute
 export default class Calculator {
   constructor(el) {
     this.name = 'Calculator';
     this.el = el;
-    // this.oResult = el.getElementsByClassName('result')[0];
-    // this.oInputs = el.getElementsByClassName('num-input');
-    // this.oBtnGroup = el.getElementsByClassName('btn-group')[0];
-
+    
     this.resultComponent = new ResultComponent();
+    this.inputGroupComponent = new InputGroupComponent()
+    this.btnGroupComponent = new BtnGroupComponent();
+
+    this.data = {
+      method: 'plus',
+      fVal: 0,
+      sVal: 0
+    };
+
+    this.selectedIndex = 0; 
   }
 
   init () {
     this.render()
-    // this.bindEvent()
+    this.bindEvent()
   }
 
   render () {
     const oFrag = document.createDocumentFragment();
 
     oFrag.appendChild(this.resultComponent.tpl());
+    oFrag.appendChild(this.inputGroupComponent.tpl());
+    oFrag.appendChild(this.btnGroupComponent.tpl());
 
     this.el.appendChild(oFrag)
   }
 
   bindEvent () {
+    const el = this.el;
+    this.oResult = el.getElementsByClassName('result')[0];
+    this.oInputs = el.getElementsByClassName('num-input');
+    this.oBtnGroup = el.getElementsByClassName('btn-group')[0];
+    this.oBtns = this.oBtnGroup.getElementsByClassName('btn'); 
+
     this.oBtnGroup.addEventListener('click', this.onBtnClick.bind(this), false);
+    this.oInputs[0].addEventListener('input', this.onInput.bind(this), false);
+    this.oInputs[1].addEventListener('input', this.onInput.bind(this), false);
   }
 
   onBtnClick (ev) {
@@ -38,12 +57,44 @@ export default class Calculator {
       tagName = tar.tagName.toLowerCase();
 
     if (tagName === 'button') {
-      const method = tar.getAttribute('data-method'),
-        fVal = digitalize(trimSpace(this.oInputs[0].value)) || 0,
-        sVal = digitalize(trimSpace(this.oInputs[1].value)) || 0;
-
-      this.setResult(method, fVal, sVal)
+      const method = tar.getAttribute('data-method');
+    //     fVal = digitalize(trimSpace(this.oInputs[0].value)) || 0,
+    //     sVal = digitalize(trimSpace(this.oInputs[1].value)) || 0;
+      this.setMethod(method);
+      this.setBtnSelected(tar)
+    //   this.setResult(method, fVal, sVal)
     }
+  }
+
+  onInput (ev) {
+    const e = ev || window.event,
+      tar = e.target || e.srcElement,
+      id = tar.getAttribute('data-id'),
+      val = digitalize(trimSpace(tar.value)) || 0;
+
+
+    switch (id) {
+      case 'fVal':
+        this.data.fVal = val;
+        break;
+      case 'sVal':
+        this.data.sVal = val;
+        break;
+      default:
+        break;
+    }
+    this.setResult(this.data.method, this.data.fVal, this.data.sVal)
+  }
+
+  setMethod(method) {
+    this.data.method = method;
+    this.setResult(this.data.method, this.data.fVal, this.data.sVal);
+  }
+
+  setBtnSelected(tar) {
+    this.oBtns[this.selectedIndex].className = 'btn';
+    this.selectedIndex = [].indexOf.call(this.oBtns, tar);
+    this.oBtns[this.selectedIndex].className += ' selected';
   }
 
   // 抽象函数，带来可扩展性，高内聚，低耦合
